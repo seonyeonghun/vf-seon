6f9f1b447ae0d1b0fb874677ddb5f60ca16d1b84
seonyeonghun_office <sprax@naver.com>
2020-08-28 13:27:54 +0900

    앱바 추가2

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,49 @@
       color="primary"
       dark
     >
+    <v-app-bar-nav-icon @click="drawer = !drawer" />
+    <v-toolbar-title>vue+firebase 웹사이트예제</v-toolbar-title>
+    <v-spacer></v-spacer>
+      <v-btn icon to="/about">
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+      <v-btn icon to="/xxx">
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
     </v-app-bar>
-
+    <v-navigation-drawer app v-model="drawer">
+      werwer
+    </v-navigation-drawer>
+    <v-content>
+      <router-view/>
+    </v-content>
+    <v-footer
+    color="primary lighten-1"
+    absolute
+    app dark
+  >
+    <v-row
+      justify="center"
+      no-gutters
+    >
+      <v-btn
+        v-for="link in links"
+        :key="link"
+        color="white"
+        text
+        rounded
+        class="my-2"
+      >
+        {{ link }}
+      </v-btn>
+      <v-col
+        class="primary lighten-2 py-4 text-center white--text"
+        cols="12"
+      >
+        {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+      </v-col>
+    </v-row>
+  </v-footer>
     <v-main>
       <HelloWorld/>
     </v-main>
@@ -18,13 +59,18 @@ import HelloWorld from './components/HelloWorld'
 
 export default {
   name: 'App',
-
+  data () {
+    return {
+      drawer: false
+    }
+  },
   components: {
     HelloWorld
-  },
+  }
+  // ,
 
-  data: () => ({
-    //
-  })
+  // data: () => ({
+  //   //
+  // })
 }
 </script>
